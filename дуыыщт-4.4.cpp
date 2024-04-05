8626ad4ae754df2a3fd51c7d910f4bb10c9067bb
Юрий Бояджиев <boyadziev@mail.ru>
2024-04-05 18:39:57 +0300

    Добавьте файлы проекта.

---
+++ дуыыщт-4.4.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x;
+	cout << "Введите число: ";
+	cin >> x;
+	x = -x; 
+	cout << "Ваше число по модулю равно: " << x;
+	return 0;
+}
